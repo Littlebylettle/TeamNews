5e4bf1a2825dfe8ae8c385d6b9caa85c3d7976cd
iseungchul <tnsrb950@gmail.com>
2023-06-30 07:19:45 +0900

    init commit

---
+++ src/main/java/com/sparta/teamnews/TeamNewsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.teamnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamNewsApplication.class, args);
+    }
+
+}
