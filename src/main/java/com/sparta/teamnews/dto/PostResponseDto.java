39ce91a6fdb3a818eb7a76232a811c0d9c8027f7
seulgi <asd0927122@gmail.com>
2023-07-04 14:02:30 +0900

    게시물 조회 기능 구현
    - 게시물 전체 조회
    - 게시물 단건 조회 ( 댓글까지 나오도록 구현 )

--- src/main/java/com/sparta/teamnews/dto/PostResponseDto.java
+++ src/main/java/com/sparta/teamnews/dto/PostResponseDto.java
@@ -4,18 +4,29 @@ import com.sparta.teamnews.entity.Post;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class PostResponseDto {
-    String title;
-    String image;
-    String username;
-    String content;
-    private LocalDateTime createAt;
+    private Long id;
+    private String title;
+    private String profilename;
+    private String content;
+    private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+    private List<CommentResponseDto> comment;
 
     public PostResponseDto(Post post){
-
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.profilename = post.getUser().getProfilename();
+        this.content = post.getContent();
+        this.createdAt = post.getCreateAt();
+        this.modifiedAt = post.getModifiedAt();
+        this.comment = post.getCommentList()
+                .stream()
+                .map(CommentResponseDto::new)
+                .toList();
 
     }
 }
--- src/main/java/com/sparta/teamnews/service/PostService.java
+++ src/main/java/com/sparta/teamnews/service/PostService.java
@@ -1,12 +1,42 @@
 package com.sparta.teamnews.service;
 
+import com.sparta.teamnews.dto.PostResponseDto;
+import com.sparta.teamnews.entity.Post;
 import com.sparta.teamnews.repository.PostRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class PostService {
     private final PostRepository postRepository;
     public PostService(PostRepository postRepository){
         this.postRepository = postRepository;
+    }
+
+
+    public List<PostResponseDto> getAllPost() {
+
+        return postRepository.findAllByOrderByCreatedAtDesc()
+                .stream()
+                .map(PostResponseDto::new)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public PostResponseDto getPost(Long id) {
+
+        Post post = findPost(id);
+
+        return new PostResponseDto(post);
+    }
+
+
+
+    private Post findPost(Long id) {
+        return postRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("선택한 게시물은 존재하지 않습니다.")
+        );
     }
 }
