61c7a3dca3cade7b6567507627924670ff4c139f
seulgi <asd0927122@gmail.com>
2023-07-04 21:03:35 +0900

    프론트엔드 파일 생성
    - index.html
    - signup.html
    - login.html

--- src/main/java/com/sparta/teamnews/TeamNewsApplication.java
+++ src/main/java/com/sparta/teamnews/TeamNewsApplication.java
@@ -2,9 +2,11 @@ package com.sparta.teamnews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+        ///(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing      //Timestamped 시간을 체크해주는 기능을 사용하기위한 애너테이션
 public class TeamNewsApplication {
 
---
+++ src/main/java/com/sparta/teamnews/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.sparta.teamnews.controller;
+
+import com.sparta.teamnews.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
